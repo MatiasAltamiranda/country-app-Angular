a3f119d216305a1ca7ebb755bddaad3ea5f554ea
maltamiranda <matias_lma94@hotmail.com>
2025-04-04 01:57:55 -0300

    Se agrega DecimalPipe en tabla de resultados para mostrar mas legible la cantidad de poblacion

--- src/app/country/components/country-table-list/country-table-list.component.ts
+++ src/app/country/components/country-table-list/country-table-list.component.ts
@@ -1,10 +1,11 @@
 import {  Component, input } from '@angular/core';
 import { RESTCountry } from '../../interfaces/rest-countries.interfaces';
 import { Country } from '../../interfaces/country.interface';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'country-table-list',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './country-table-list.component.html',
 })
 export class CountryTableListComponent {
